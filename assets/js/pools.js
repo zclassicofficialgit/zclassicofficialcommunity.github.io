0f925606e99c2185f52ad2a657dc8f3102556b02
lelonex <pglmnl@yahoo.it>
2019-09-06 21:27:01 +0200

    fix pools hashrate percentages

--- assets/js/pools.js
+++ assets/js/pools.js
@@ -11,10 +11,15 @@ $(document).ready( function () {
 			$(".poolminers").html(data.poolsminers)
 			var poolHtml = '<thead><tr><th></th><th>pool</th><th>country</th><th>% hash</th><th>hashrate (MSol/s)</th></tr></thead><tbody>';
 			var pools = data.data.sort(function(a,b) { return b.hashrate - a.hashrate});
+			var poolhashrate = 0;
+			for (var i=0; i < pools.length; i++) {
+				var pool = data.data[i];
+				poolhashrate += pool.hashrate;
+			}
 			for (var i=0; i < pools.length; i++) {
 				var pool = data.data[i];
 				if (pool.hashrate > 0) {
-					poolHtml += '<tr><td class="center"><img width="20px" src="/assets/images/flags/'+pool.country.split('|')[0].toLowerCase()+'.png" alt="'+pool.country.split('|')[0]+'"/></td><td><a href="'+pool.url+'" target="_blank">' + pool.pool_id + '</td><td>' + pool.country.split('|')[1] +'</td><td class="right">'+((pool.hashrate/data.poolshash)*100).toFixed(2)+'%</td><td class="right">' + (pool.hashrate/1000000).toFixed(4) + '</td></tr>';
+					poolHtml += '<tr><td class="center"><img width="20px" src="/assets/images/flags/'+pool.country.split('|')[0].toLowerCase()+'.png" alt="'+pool.country.split('|')[0]+'"/></td><td><a href="'+pool.url+'" target="_blank">' + pool.pool_id + '</td><td>' + pool.country.split('|')[1] +'</td><td class="right">'+((pool.hashrate/poolhashrate)*100).toFixed(2)+'%</td><td class="right">' + (pool.hashrate/1000000).toFixed(4) + '</td></tr>';
 				}
 			}
 			poolHtml += '</tbody>';

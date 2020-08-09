cc3b812340fdf28c916e2b06f6fe284e65ee45f3
lelonex <pglmnl@yahoo.it>
2020-08-09 14:30:13 +0200

    disable market info

--- assets/js/markets.js
+++ assets/js/markets.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-	$.getJSON("//zclassic.mindland.it/services/contents/json.php?url=https://coinmarketcap.com/currencies/zclassic/&callback=?", function(data) {
-		if (data) {
-
+/**
+	$.getJSON("/services/contents/json.php?url=https://coinmarketcap.com/currencies/zclassic/&callback=?", function(data) {
+		if (data) {  		   
 			var marketHtmlIdx = data.content.indexOf('<div class="cmc-table__table-wrapper-outer"><div><table>');
-		    // var marketHtmlIdx = data.content.indexOf('<table id="markets-table"');
 			var marketHtml = data.content.substring(marketHtmlIdx,data.content.indexOf('</table>',marketHtmlIdx) + 8);
 			marketHtml = marketHtml.replace(/data-src=\"http/g,'src="http');
 			marketHtml = marketHtml.replace(/src=\"data/g,'data-src="data');
@@ -11,13 +10,13 @@ $(document).ready(function() {
 			$("#marketstable").html(marketHtml);
 			$("#marketstable").DataTable({responsive: true, paging: false, searching: false, info: false, ordering: false});	
 
-		    /**
+		    
 			var summaryHtmlIdx = data.content.indexOf('<table class="table cmc-table-striped cmc-cc-summary-table"');
 			var summaryHtml = data.content.substring(summaryHtmlIdx ,data.content.indexOf('</table>',summaryHtmlIdx) + 8);
 			$(".summary").show();
 			$("#summarytable").html(summaryHtml);
 			$("#summarytable").DataTable({responsive: true, paging: false, searching: false, info: false, ordering: false});	
-		    */
 		}
 	});
+**/
 });
